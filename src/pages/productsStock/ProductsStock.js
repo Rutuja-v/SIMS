9415d0fe63781cb60b5b21538ad598cf86814973
AchuthanR <r.achuthan2001@gmail.com>
2023-05-10 12:04:11 +0530

    Bug fix

--- src/pages/productsStock/ProductsStock.js
+++ src/pages/productsStock/ProductsStock.js
@@ -37,87 +37,96 @@ function ProductsStock({ godown }) {
       });
   };
 
+  let children;
+
+  if (godown === undefined || godown === null) {
+    children = <Grid item>Oops! You are not mapped to a godown yet.</Grid>
+  }
+  else if (productsStock.length === 0) {
+    children = <Grid item>Oops! No products are in stock in this godown!</Grid>;
+  }
+  else {
+    children = productsStock.map((productsStock) => (
+      <Grid item xs={12} sm={6} md={4} key={productsStock.product.id}>
+        <Card>
+          <CardMedia
+            style={{ height: "180px" }}
+            component="img"
+            image="https://www.cassidybros.ie/wp-content/uploads/2020/11/product-placeholder.jpg"
+            title="product image"
+          />
+          <CardContent
+            style={{
+              padding: "16px 24px",
+            }}
+          >
+            <Typography
+              gutterBottom
+              style={{ fontWeight: "bold" }}
+              variant="h6"
+              component="h4"
+            >
+              {productsStock.product.name}
+            </Typography>
+
+            <div
+              style={{
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+                flexWrap: "wrap",
+                gap: "10px",
+              }}
+            >
+              <div>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {"Price:"}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {productsStock.product.price}
+                </Typography>
+              </div>
+
+              <div>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {"Stock:"}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {productsStock.stock}
+                </Typography>
+              </div>
+
+              <div>
+                <Typography
+                  variant="caption"
+                  color="textSecondary"
+                  component="p"
+                >
+                  {"Weight (in quintals):"}
+                </Typography>
+                <Typography variant="body2" component="p">
+                  {productsStock.product.weight}
+                </Typography>
+              </div>
+            </div>
+          </CardContent>
+        </Card>
+      </Grid>
+    ))
+  }
+
   return (
     <div className="App">
       <Grid container spacing={6}>
-        {(godown === undefined || godown === null) ?
-          <Grid item>Oops! No products are in stock in this godown!</Grid>
-          : productsStock.map((productsStock) => (
-            <Grid item xs={12} sm={6} md={4} key={productsStock.product.id}>
-              <Card>
-                <CardMedia
-                  style={{ height: "180px" }}
-                  component="img"
-                  image="https://www.cassidybros.ie/wp-content/uploads/2020/11/product-placeholder.jpg"
-                  title="product image"
-                />
-                <CardContent
-                  style={{
-                    padding: "16px 24px",
-                  }}
-                >
-                  <Typography
-                    gutterBottom
-                    style={{ fontWeight: "bold" }}
-                    variant="h6"
-                    component="h4"
-                  >
-                    {productsStock.product.name}
-                  </Typography>
-
-                  <div
-                    style={{
-                      display: "flex",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      flexWrap: "wrap",
-                      gap: "10px",
-                    }}
-                  >
-                    <div>
-                      <Typography
-                        variant="caption"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {"Price:"}
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        {productsStock.product.price}
-                      </Typography>
-                    </div>
-
-                    <div>
-                      <Typography
-                        variant="caption"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {"Stock:"}
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        {productsStock.stock}
-                      </Typography>
-                    </div>
-
-                    <div>
-                      <Typography
-                        variant="caption"
-                        color="textSecondary"
-                        component="p"
-                      >
-                        {"Weight (in quintals):"}
-                      </Typography>
-                      <Typography variant="body2" component="p">
-                        {productsStock.product.weight}
-                      </Typography>
-                    </div>
-                  </div>
-                </CardContent>
-              </Card>
-            </Grid>
-          ))
-        }
+        {children}
       </Grid>
     </div>
   );
