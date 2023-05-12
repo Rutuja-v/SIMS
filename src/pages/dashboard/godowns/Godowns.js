f5338f35c463538b9fa39d8e7c6a7332d6ca9289
AchuthanR <r.achuthan2001@gmail.com>
2023-05-12 14:15:05 +0530

    Fix bug

--- src/pages/dashboard/godowns/Godowns.js
+++ src/pages/dashboard/godowns/Godowns.js
@@ -200,6 +200,7 @@ function Godowns() {
         role: {
           id: values.managerRoleId,
         },
+        isLocked: false,
       };
 
       const startDateObj = new Date(values.startDate);
